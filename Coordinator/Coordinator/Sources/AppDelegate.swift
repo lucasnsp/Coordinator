8c148dd170075012c7f5ab2b307dfeecb067c49a
Lucas Neves <lucas.pompeu02@hotmail.com>
2024-03-26 20:07:05 -0300

    Resolving coordinator problem.

--- Coordinator/Coordinator/Sources/AppDelegate.swift
+++ Coordinator/Coordinator/Sources/AppDelegate.swift
@@ -13,11 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let appCoordinator = AppCoordinator()
-        appCoordinator.start()
-        window?.rootViewController = appCoordinator.navigationController
-        window?.makeKeyAndVisible()
         return true
     }
 
